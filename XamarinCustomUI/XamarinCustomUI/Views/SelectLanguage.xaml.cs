bfd41ca8b9f55073fb5d2e798c389741e958ed4e
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2020-12-11 12:22:26 +0530

    Select Language page added.

---
+++ XamarinCustomUI/XamarinCustomUI/Views/SelectLanguage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinCustomUI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectLanguage : ContentPage
+    {
+        public SelectLanguage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(langPicker.SelectedItem!=null)
+            {
+                var language = CultureInfo.GetCultures(CultureTypes.NeutralCultures).ToList().First(es => es.EnglishName.Contains(langPicker.SelectedItem.ToString()));
+                Thread.CurrentThread.CurrentCulture = language;
+
+            }
+        }
+    }
+}
