e382127e5b53e8fc27afe1d4f0e2c95941e984d4
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2020-12-19 23:14:21 +0530

    SegmentedButtonCollection added.

--- XamarinCustomUI/XamarinCustomUI/Controls/SegmentedButtonCollection.cs
+++ XamarinCustomUI/XamarinCustomUI/Controls/SegmentedButtonCollection.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 
 namespace XamarinCustomUI.Controls
 {
-    class SegmentedButtonCollection
+    public class SegmentedButtonCollection : ICollection<SegmentedButton>
     {
+        private List<SegmentedButton> _segmentedButtons = new List<SegmentedButton>();
+
+        public int Count => _segmentedButtons.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(SegmentedButton item)
+        {
+            _segmentedButtons.Add(item);
+        }
+
+        public void Clear()
+        {
+            _segmentedButtons.Clear();
+        }
+
+        public bool Contains(SegmentedButton item)
+        {
+            return _segmentedButtons.Contains(item);
+        }
+
+        public void CopyTo(SegmentedButton[] array, int arrayIndex)
+        {
+            _segmentedButtons.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<SegmentedButton> GetEnumerator()
+        {
+            return _segmentedButtons.GetEnumerator();
+        }
+
+        public bool Remove(SegmentedButton item)
+        {
+            return _segmentedButtons.Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _segmentedButtons.GetEnumerator();
+        }
     }
 }
