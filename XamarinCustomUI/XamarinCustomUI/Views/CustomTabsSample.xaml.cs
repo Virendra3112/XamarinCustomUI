0dce23402a132d83f3206fbcb1efebe7880c65f8
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2020-12-21 12:04:07 +0530

    CustomTabsSample updated.

--- XamarinCustomUI/XamarinCustomUI/Views/CustomTabsSample.xaml.cs
+++ XamarinCustomUI/XamarinCustomUI/Views/CustomTabsSample.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +8,63 @@ namespace XamarinCustomUI.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CustomTabsSample : ContentPage
     {
+        public static readonly BindableProperty SelectedTabProperty = BindableProperty.Create(nameof(SelectedTab), returnType: typeof(int), declaringType: typeof(int), defaultValue: 0);
+
+        public int SelectedTab
+        {
+            get
+            {
+                return (int)GetValue(SelectedTabProperty);
+            }
+            set
+            {
+                SetValue(SelectedTabProperty, value);
+            }
+        }
+
+
+        //public Command BackButtonTapped { get; set; }
+        //public Command BellButtonTapped { get; set; }
+
         public CustomTabsSample()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            //BackButtonTapped = new Command(OnBackButtonTapped);
+            //BellButtonTapped = new Command(OnBellButtonTapped);
         }
+
+        ///// <summary>
+        ///// On Back Button Tapped
+        ///// </summary>
+        //private void OnBackButtonTapped()
+        //{
+        //    try
+        //    {
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //    }
+        //}
+
+        ///// <summary>
+        ///// On Bell Button Tapped
+        ///// </summary>
+        //private void OnBellButtonTapped()
+        //{
+        //    try
+        //    {
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //    }
+        //}
+
     }
 }
