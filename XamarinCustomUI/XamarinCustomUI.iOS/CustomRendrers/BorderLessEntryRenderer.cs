20d1d3991a131e8da6f7479a58885fd95e85e77f
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2020-12-25 15:05:29 +0530

    CustomEntryRenderer updated (iOS)

--- XamarinCustomUI/XamarinCustomUI.iOS/CustomRendrers/BorderLessEntryRenderer.cs
+++ XamarinCustomUI/XamarinCustomUI.iOS/CustomRendrers/BorderLessEntryRenderer.cs
@@ -1,14 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using CoreAnimation;
 
-using Foundation;
 using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinCustomUI.Controls;
+using XamarinCustomUI.iOS.CustomRendrers;
 
+[assembly: ExportRenderer(typeof(BorderLessEntry), typeof(CustomEntryRenderer))]
 namespace XamarinCustomUI.iOS.CustomRendrers
 {
-    class BorderLessEntryRenderer
+    public class CustomEntryRenderer : EntryRenderer
     {
+        private CALayer _line;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            try
+            {
+
+                base.OnElementChanged(e);
+                _line = null;
+                if (Control == null || e.NewElement == null)
+                    return;
+                Control.Layer.BorderWidth = 0;
+                //Control.Layer.BorderWidth = 600;
+                Control.BorderStyle = UITextBorderStyle.None;
+                _line = new CALayer
+                {
+                    // BorderColor = UIColor.FromRGB(0, 40, 85).CGColor,
+                    //BackgroundColor = UIColor.FromRGB(0, 40, 85).CGColor,
+                    //Frame = new CGRect(0, Frame.Height / 2, Frame.Width * 2, 1f)
+                    // BorderStyle
+                    Frame = new CoreGraphics.CGRect(0, Frame.Height, UIScreen.MainScreen.Bounds.Size.Width, 1f)
+
+                };
+                Control.Layer.AddSublayer(_line);
+            }
+            catch (System.Exception ex)
+            {
+                ex.ToString();
+            }
+        }
     }
 }
