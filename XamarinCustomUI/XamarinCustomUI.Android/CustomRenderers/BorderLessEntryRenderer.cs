5962525580413bcc719e265b4e3a5793e8f7ac8f
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2020-12-25 15:04:58 +0530

    CustomEntryRenderer updated (Android)

--- XamarinCustomUI/XamarinCustomUI.Android/CustomRenderers/BorderLessEntryRenderer.cs
+++ XamarinCustomUI/XamarinCustomUI.Android/CustomRenderers/BorderLessEntryRenderer.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinCustomUI.Controls;
+using XamarinCustomUI.Droid.CustomRenderers;
 
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+[assembly: ExportRenderer(typeof(BorderLessEntry), typeof(BorderLessEntryRenderer))]
 
 namespace XamarinCustomUI.Droid.CustomRenderers
 {
-    class BorderLessEntryRenderer
+    public class BorderLessEntryRenderer : EntryRenderer
     {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Argb(0, 0, 0, 0));
+            }
+        }
     }
 }
