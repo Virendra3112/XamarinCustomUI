24e4b81491091e4f02a21be43b050a95d1fd590e
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2020-12-27 13:00:08 +0530

    CustomGalleryView updated.

---
+++ XamarinCustomUI/XamarinCustomUI/Models/GalleryModel.cs
@@ -0,0 +1,22 @@
+﻿namespace XamarinCustomUI.Models
+{
+    public class GalleryModel
+    {
+        public int ImageId
+        {
+            get; set;
+        }
+        public string ImageUrl
+        {
+            get; set;
+        }
+        public bool IsDeleted
+        {
+            get; set;
+        }
+        public bool IsSelected
+        {
+            get; set;
+        }
+    }
+}
--- XamarinCustomUI/XamarinCustomUI/Views/CustomGalleryView.xaml.cs
+++ XamarinCustomUI/XamarinCustomUI/Views/CustomGalleryView.xaml.cs
@@ -1,20 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinCustomUI.Models;
 
 namespace XamarinCustomUI.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CustomGalleryView : ContentPage
+    public partial class CustomGalleryView : ContentPage, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+        private ObservableCollection<GalleryModel> _galleryList;
+
+        public ObservableCollection<GalleryModel> GalleryList
+        {
+            get { return _galleryList; }
+            set { _galleryList = value; NotifyPropertyChanged(); }
+        }
+
         public CustomGalleryView()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GalleryList = new ObservableCollection<GalleryModel>();
         }
     }
 }
