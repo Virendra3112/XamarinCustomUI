c4f6f87372a5df754124788d523b2d33d0096360
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2020-12-28 12:42:15 +0530

    SingleClickEffect added.

---
+++ XamarinCustomUI/XamarinCustomUI/Effects/SingleClickEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+
+namespace XamarinCustomUI.Effects
+{
+    public class SingleClickEffect : RoutingEffect
+    {
+        public SingleClickEffect() : base("MyApp.SingleClickEffect")
+        {
+        }
+
+        public static readonly BindableProperty CommandProperty = BindableProperty.CreateAttached("Command", typeof(ICommand), typeof(SingleClickEffect), (object)null);
+        public static ICommand GetCommand(BindableObject view)
+        {
+            return (ICommand)view.GetValue(CommandProperty);
+        }
+
+        public static void SetCommand(BindableObject view, ICommand value)
+        {
+            view.SetValue(CommandProperty, value);
+        }
+
+
+        public static readonly BindableProperty CommandParameterProperty = BindableProperty.CreateAttached("CommandParameter", typeof(object), typeof(SingleClickEffect), (object)null);
+        public static object GetCommandParameter(BindableObject view)
+        {
+            return view.GetValue(CommandParameterProperty);
+        }
+
+        public static void SetCommandParameter(BindableObject view, object value)
+        {
+            view.SetValue(CommandParameterProperty, value);
+        }
+    }
+}
