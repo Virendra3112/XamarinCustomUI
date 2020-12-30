57a51068303a6ca41f257743a0daaa8aafcdad43
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2020-12-30 18:52:43 +0530

    iOSLongPressedEffect added.

---
+++ XamarinCustomUI/XamarinCustomUI.iOS/Effects/iOSLongPressedEffect.cs
@@ -0,0 +1,58 @@
+﻿
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinCustomUI.iOS.Effects;
+
+[assembly: ResolutionGroupName("MyApp")]
+[assembly: ExportEffect(typeof(iOSLongPressedEffect), "LongPressedEffect")]
+namespace XamarinCustomUI.iOS.Effects
+{
+    public class iOSLongPressedEffect : PlatformEffect
+    {
+        private bool _attached;
+        private readonly UILongPressGestureRecognizer _longPressRecognizer;
+        /// <summary>
+        /// Initializes a new instance of the
+        /// </summary>
+        public iOSLongPressedEffect()
+        {
+            _longPressRecognizer = new UILongPressGestureRecognizer(HandleLongClick);
+        }
+
+        /// <summary>
+        /// Apply the handler
+        /// </summary>
+        protected override void OnAttached()
+        {
+            //because an effect can be detached immediately after attached (happens in listview), only attach the handler one time
+            if (!_attached)
+            {
+                Container.AddGestureRecognizer(_longPressRecognizer);
+                _attached = true;
+            }
+        }
+
+        /// <summary>
+        /// Invoke the command if there is one
+        /// </summary>
+        private void HandleLongClick()
+        {
+            var command = LongPressedEffect.GetCommand(Element);
+            command?.Execute(LongPressedEffect.GetCommandParameter(Element));
+        }
+
+        /// <summary>
+        /// Clean the event handler on detach
+        /// </summary>
+        protected override void OnDetached()
+        {
+            if (_attached)
+            {
+                Container.RemoveGestureRecognizer(_longPressRecognizer);
+                _attached = false;
+            }
+        }
+
+    }
+}
