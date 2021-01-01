b142efaeefbf681b410abed0fb04112b2e734e78
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-01 13:14:18 +0530

    RoundedCornersButton added.

---
+++ XamarinCustomUI/XamarinCustomUI/Controls/RoundedCornersButton.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinCustomUI.Controls
+{
+    public class RoundedCornersButton : ContentView
+    {
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(RoundedCornersButton), default(float));
+        public static readonly BindableProperty BorderThicknessProperty = BindableProperty.Create(nameof(BorderThickness), typeof(int), typeof(RoundedCornersButton), default(int));
+        public static readonly BindableProperty BorderColorProperty = BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(RoundedCornersButton), default(Color));
+
+        public float CornerRadius
+        {
+            get { return (float)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public int BorderThickness
+        {
+            get { return (int)GetValue(BorderThicknessProperty); }
+            set { SetValue(BorderThicknessProperty, value); }
+        }
+
+        public Color BorderColor
+        {
+            get { return (Color)GetValue(BorderColorProperty); }
+            set { SetValue(BorderColorProperty, value); }
+        }
+    }
+}
