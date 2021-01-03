eb6a610da2a4d1cd02f29c339637060c684a584a
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-03 11:31:16 +0530

    CustomWebViewRenderer added for iOS.

---
+++ XamarinCustomUI/XamarinCustomUI.iOS/CustomRendrers/CustomWebViewRenderer.cs
@@ -0,0 +1,36 @@
+﻿using Foundation;
+using System.IO;
+using System.Net;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinCustomUI.Controls;
+using XamarinCustomUI.iOS.CustomRendrers;
+
+[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace XamarinCustomUI.iOS.CustomRendrers
+{
+    public class CustomWebViewRenderer : ViewRenderer<CustomWebView, UIWebView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<CustomWebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                SetNativeControl(new UIWebView());
+            }
+            if (e.OldElement != null)
+            {
+                // Cleanup
+            }
+            if (e.NewElement != null)
+            {
+                var customWebView = Element as CustomWebView;
+                string fileName = Path.Combine(NSBundle.MainBundle.BundlePath, string.Format("Content/{0}", WebUtility.UrlEncode(customWebView.Uri)));
+                Control.LoadRequest(new NSUrlRequest(new NSUrl(fileName, false)));
+                Control.ScalesPageToFit = true;
+            }
+        }
+    }
+}
