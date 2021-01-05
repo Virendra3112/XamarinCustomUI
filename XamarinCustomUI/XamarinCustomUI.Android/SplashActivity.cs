2b8d6bef8da19e4415685d31460753967599fe64
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-05 15:38:14 +0530

    Splash updated.

--- XamarinCustomUI/XamarinCustomUI.Android/SplashActivity.cs
+++ XamarinCustomUI/XamarinCustomUI.Android/SplashActivity.cs
@@ -13,31 +13,36 @@ namespace XamarinCustomUI.Droid
          )]
     public class SplashActivity : Activity
     {
+        //protected override void OnCreate(Bundle savedInstanceState)
+        //{
+        //    RequestWindowFeature(WindowFeatures.NoTitle);
+        //    base.OnCreate(savedInstanceState);
+        //    SetContentView(Resource.Layout.SplashLayout);
+        //    if (Build.VERSION.SdkInt >= BuildVersionCodes.Kitkat)
+        //    {
+        //        this.Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+        //    }
+        //}
+
+        //protected override void OnResume()
+        //{
+        //    base.OnResume();
+        //    Task startupWork = new Task(SimulateStartup);
+        //    startupWork.Start();
+        //}
+
+        //private async void SimulateStartup()
+        //{
+        //    await Task.Delay(1000);
+        //    StartActivityForResult(new Intent(this, typeof(MainActivity)), 0);
+        //}
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            RequestWindowFeature(WindowFeatures.NoTitle);
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.SplashLayout);
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.Kitkat)
-            {
-                this.Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
-            }
-           // var splashTxt = FindViewById<TextView>(Resource.Id.texto);
-            //var typeface = Typeface.CreateFromAsset(this.Assets, "Tajawal-Regular.ttf");
-            //splashTxt.Typeface = typeface;
-        }
 
-        protected override void OnResume()
-        {
-            base.OnResume();
-            Task startupWork = new Task(SimulateStartup);
-            startupWork.Start();
-        }
-
-        private async void SimulateStartup()
-        {
-            await Task.Delay(1000);
-            StartActivityForResult(new Intent(this, typeof(MainActivity)), 0);
+            // Create your application here
+            StartActivity(typeof(MainActivity));
         }
     }
 }
