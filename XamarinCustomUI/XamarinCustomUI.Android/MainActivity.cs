3c06a86b21e5439af632fd14258448106adfb3ca
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-05 15:15:10 +0530

    Splash added for Android

--- XamarinCustomUI/XamarinCustomUI.Android/MainActivity.cs
+++ XamarinCustomUI/XamarinCustomUI.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android;
 
 namespace XamarinCustomUI.Droid
 {
-    [Activity(Label = "XamarinCustomUI", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "XamarinCustomUI", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ XamarinCustomUI/XamarinCustomUI.Android/SplashActivity.cs
@@ -0,0 +1,43 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+
+namespace XamarinCustomUI.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash",
+          MainLauncher = true
+         )]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SplashLayout);
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Kitkat)
+            {
+                this.Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+            }
+           // var splashTxt = FindViewById<TextView>(Resource.Id.texto);
+            //var typeface = Typeface.CreateFromAsset(this.Assets, "Tajawal-Regular.ttf");
+            //splashTxt.Typeface = typeface;
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(SimulateStartup);
+            startupWork.Start();
+        }
+
+        private async void SimulateStartup()
+        {
+            await Task.Delay(1000);
+            StartActivityForResult(new Intent(this, typeof(MainActivity)), 0);
+        }
+    }
+}
