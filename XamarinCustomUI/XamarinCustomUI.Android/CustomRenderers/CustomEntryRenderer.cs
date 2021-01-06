cdc5975e2f060548832019ccbb80587e04cbeb3e
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-06 23:00:26 +0530

    CustomEntryRenderer added for android.

---
+++ XamarinCustomUI/XamarinCustomUI.Android/CustomRenderers/CustomEntryRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinCustomUI.Controls;
+using XamarinCustomUI.Droid.CustomRenderers;
+
+[assembly: ExportRenderer(typeof(BorderLessEntry), typeof(CustomEntryRenderer))]
+namespace XamarinCustomUI.Droid.CustomRenderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Argb(0, 0, 0, 0));
+            }
+        }
+    }
+}
