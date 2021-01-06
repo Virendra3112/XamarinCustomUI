664db0c29ee5e1d2818ca8cd6db6c0a126a43fab
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-06 23:08:52 +0530

    CustomCachedImage_SVG added.

---
+++ XamarinCustomUI/XamarinCustomUI/Controls/CustomCachedImage_SVG.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using FFImageLoading.Svg.Forms;
+using Xamarin.Forms;
+
+namespace XamarinCustomUI.Controls
+{
+    public class CustomCachedImage_SVG : SvgCachedImage
+    {
+        public static readonly BindableProperty ImageColorProperty =
+            BindableProperty.Create("ImageColor", typeof(Color), typeof(CustomCachedImage_SVG), Application.Current.Resources["secondaryColor"]);
+
+        public Color ImageColor
+        {
+            get { return (Color)GetValue(ImageColorProperty); }
+            set { SetValue(ImageColorProperty, value); }
+        }
+
+        public CustomCachedImage_SVG()
+        {
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == "ImageColor")
+            {
+                SetColor();
+            }
+        }
+
+        private void SetColor()
+        {
+            //Dictionary<string, string> imageStringMap = new Dictionary<string, string>()
+            //{
+            //    {
+            //       AppConstants.CustomSvgCachedImage.DefaultFillColor,
+            //       string.Format(AppConstants.CustomSvgCachedImage.FillColor, ImageColor.ToHex())
+            //    }
+            //};
+
+            //ReplaceStringMap = imageStringMap;
+            //Source = Source;
+        }
+    }
+}
+
