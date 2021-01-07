25c9d7214e69badcd364e039988fe45d5fea5c90
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-07 13:49:54 +0530

    ApplicationConstants added.

---
+++ XamarinCustomUI/XamarinCustomUI/Helpers/ApplicationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace XamarinCustomUI.Helpers
+{
+    public static class ApplicationConstants
+    {
+        public const string DefaultFillColor = "fill=\"#ffffff00\"";
+        public const string FillColor = "fill=\"{0}\"";
+
+    }
+}
