883d7b1223e209d73f2e883a1f4643a84f0848fe
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-16 23:48:43 +0530

    MultiMediaPickerService, IMultiMediaPickerService added.

---
+++ XamarinCustomUI/XamarinCustomUI.Android/CustomRenderers/MultiMediaPickerService .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XamarinCustomUI.Helpers;
+using XamarinCustomUI.Models;
+
+namespace XamarinCustomUI.Droid.CustomRenderers
+{
+    public class MultiMediaPickerService : IMultiMediaPickerService
+    {
+        public event EventHandler<MediaFile> OnMediaPicked;
+        public event EventHandler<IList<MediaFile>> OnMediaPickedCompleted;
+
+        public void Clean()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<MediaFile>> PickPhotosAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<MediaFile>> PickVideosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XamarinCustomUI/XamarinCustomUI.iOS/CustomRendrers/MultiMediaPickerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+using XamarinCustomUI.Helpers;
+using XamarinCustomUI.Models;
+
+namespace XamarinCustomUI.iOS.CustomRendrers
+{
+    public class MultiMediaPickerService : IMultiMediaPickerService
+    {
+        public event EventHandler<MediaFile> OnMediaPicked;
+        public event EventHandler<IList<MediaFile>> OnMediaPickedCompleted;
+
+        public void Clean()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<MediaFile>> PickPhotosAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<MediaFile>> PickVideosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
