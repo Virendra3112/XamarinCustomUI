d9de89bd3f598834d4f9a8477f78420a131b4497
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-17 23:22:35 +0530

    MultipleImagePicker page updated.

--- XamarinCustomUI/XamarinCustomUI/Views/MultipleImagePicker.xaml.cs
+++ XamarinCustomUI/XamarinCustomUI/Views/MultipleImagePicker.xaml.cs
@@ -1,20 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinCustomUI.Helpers;
+using XamarinCustomUI.Models;
 
 namespace XamarinCustomUI.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MultipleImagePicker : ContentPage
     {
+        IMultiMediaPickerService _multiMediaPickerService;
+
+        public ObservableCollection<MediaFile> Media { get; set; }
+        public ICommand SelectImagesCommand { get; set; }
+        public ICommand SelectVideosCommand { get; set; }
         public MultipleImagePicker()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
