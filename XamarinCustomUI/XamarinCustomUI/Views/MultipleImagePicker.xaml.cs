13c5b394b7b0432c45bf510f74b1a9c16b0b0800
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-17 23:28:55 +0530

    MultipleImagePicker updated.

--- XamarinCustomUI/XamarinCustomUI/Views/MultipleImagePicker.xaml.cs
+++ XamarinCustomUI/XamarinCustomUI/Views/MultipleImagePicker.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -23,15 +25,74 @@ namespace XamarinCustomUI.Views
         public MultipleImagePicker()
         {
             InitializeComponent();
-        }
 
-        private void Button_Clicked(object sender, EventArgs e)
-        {
+            _multiMediaPickerService = DependencyService.Get<IMultiMediaPickerService>();//.PickPhotosAsync();
 
         }
 
-        private void Button_Clicked_1(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
+            var hasPermission = await CheckPermissionsAsync();
+            if (hasPermission)
+            {
+                Media = new ObservableCollection<MediaFile>();
+
+                await _multiMediaPickerService.PickPhotosAsync();
+
+                _multiMediaPickerService.OnMediaPicked += (s, a) =>
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        Media.Add(a);
+
+                    });
+                };
+            }
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var hasPermission = await CheckPermissionsAsync();
+            if (hasPermission)
+            {
+                await _multiMediaPickerService.PickVideosAsync();
+            }
+        }
+
+        async Task<bool> CheckPermissionsAsync()
+        {
+            var retVal = false;
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Plugin.Permissions.Abstractions.Permission.Storage))
+                    {
+                        await App.Current.MainPage.DisplayAlert("Alert", "Need Storage permission to access to your photos.", "Ok");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Storage });
+                    status = results[Plugin.Permissions.Abstractions.Permission.Storage];
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    retVal = true;
+
+                }
+                else if (status != PermissionStatus.Unknown)
+                {
+                    await App.Current.MainPage.DisplayAlert("Alert", "Permission Denied. Can not continue, try again.", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                await App.Current.MainPage.DisplayAlert("Alert", "Error. Can not continue, try again.", "Ok");
+            }
+
+            return retVal;
 
         }
     }
